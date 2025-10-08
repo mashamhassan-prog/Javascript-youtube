554f92eef534ce367bc6e0e20f39ee980c19c9d2
Muhammad Masham Hassan <hassanmasham789@gmail.com>
2025-10-08 21:05:44 +0000

    Datatypes discussed

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strcit"; // treat all JS code as a newer version
+
+/*alert( 3 + 3 )*/ 
+// we are using Nodejs, not a Browser, that's why it gives an error!
+
+// now we run it with console
+
+/*console.log( 3 
++ 3 )*/        //we can write the code like this too but by managing the code makes code readable!!!
+console.log( 3 + 3 )
+//here we also talk about the code readability which should be high
+
+console.log("Masham")
