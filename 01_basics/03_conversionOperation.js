2ada89e3d835e733ca8ad310b1807ca3b2850a3f
Muhammad Masham Hassan <hassanmasham789@gmail.com>
2025-10-08 12:54:16 +0000

    Operations discussed

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+/*let score = 33*/
+// this will called 'number' when we execute the output value
+
+// /* if we write the number in the ""*/
+// let score = "33"
+// this will called 'string' when we execute the output value
+
+// if we write the number with the aplhabet
+// let score = "33abc"
+// this will called "NaN" which is known as not a number
+
+// if we give the value of "null"
+// let score = null
+// then it will return "0"
+
+// if we give the value of "undefined"
+let score = undefined
+// then it will return ""
+
+// here we have two kinds of writing console for the output
+console.log(typeof score);
+console.log(typeof(score));
+
+// here we can declare a new variable for the execution of the number (output)
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+// it declares the type
+
+console.log(valueInNumber)
+// it is for the execution of the value
