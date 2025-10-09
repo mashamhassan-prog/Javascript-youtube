5282ddedba521cba32c5a25744b8ede1f30d90d9
Muhammad Masham Hassan <hassanmasham789@gmail.com>
2025-10-09 14:46:16 +0000

    Operations discussed

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -2,20 +2,34 @@
 // this will called 'number' when we execute the output value
 
 // /* if we write the number in the ""*/
+
 // let score = "33"
 // this will called 'string' when we execute the output value
 
 // if we write the number with the aplhabet
+
 // let score = "33abc"
 // this will called "NaN" which is known as not a number
 
 // if we give the value of "null"
-// let score = null
+
+// let score = nul
 // then it will return "0"
 
 // if we give the value of "undefined"
-let score = undefined
-// then it will return ""
+
+// let score = undefined
+// then it will return "NaN"
+
+// if we give the boolean value like "true" or "false"
+
+// let score = false
+// then it will return "1" for true and "0" for false
+
+// and for example there's a string which cannot be convert into the number
+
+let score = "Masham"
+// then it will return "NaN" coz Masham is not a number tho
 
 // here we have two kinds of writing console for the output
 console.log(typeof score);
@@ -28,3 +42,13 @@ console.log(typeof valueInNumber);
 
 console.log(valueInNumber)
 // it is for the execution of the value
+
+
+// *Notes for conversion*
+
+// "33" => 33
+// "33abc" => NaN(but the type of NaN is a "number")
+// true => 1; false => 0
+
+/*these are basic conversions*/
+
