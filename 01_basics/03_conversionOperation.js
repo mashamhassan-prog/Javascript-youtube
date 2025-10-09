eac210b74ee31f050a2cf17c5c7938eee7da9aff
Muhammad Masham Hassan <hassanmasham789@gmail.com>
2025-10-09 15:08:12 +0000

    Conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,5 @@
+// NUMBER CONVERSION 
+
 /*let score = 33*/
 // this will called 'number' when we execute the output value
 
@@ -28,19 +30,19 @@
 
 // and for example there's a string which cannot be convert into the number
 
-let score = "Masham"
+// let score = "Masham"
 // then it will return "NaN" coz Masham is not a number tho
 
 // here we have two kinds of writing console for the output
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 // here we can declare a new variable for the execution of the number (output)
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
 // it declares the type
 
-console.log(valueInNumber)
+// console.log(valueInNumber)
 // it is for the execution of the value
 
 
@@ -51,4 +53,37 @@ console.log(valueInNumber)
 // true => 1; false => 0
 
 /*these are basic conversions*/
+
+
+// More conversions
+// BOOLEAN CONVERSION 
+// now here we give variable a value and we want to convert it through boolean expression like in true or false
+// let isLoggedIn = 1
+// here variable(isLoggedIn) having a value of 1 we apply boolean conversion:
+// and it comes true!!! ; if there's is "0" then it come false!!!
+
+// let isLoggedIn = ""
+// here variable(isLoggedIn) having a no value we apply boolean conversion:
+// then it comes false!!!
+
+// let isLoggedIn = "Masham"
+// here variable(isLoggedIn) having a value "masham" we apply boolean conversion:
+// then it comes true!!!
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// we declare new variable of Booleanisloggedin and we want to convert its value in boolean we give it with its value 
+// console.log(booleanIsLoggedIn);
+// here for the execution we write the variable name in the console which we have to execute!
+
+
+// Now,
+// STRING CONVERSION
+let someNumber = 33 
+// it will return "33"
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
+// here we wanted to know that it's a string or not!
 
