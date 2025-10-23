2133332d34799dc51fd2e1b72a07680ba5be302d
Muhammad Masham Hassan <hassanmasham789@gmail.com>
2025-10-23 10:58:37 +0000

    discussing comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,20 @@
+// here we want know about the comparisons in JS
+// for EXAMPLE
+
+// console.log(2 > 1);
+// 2 greater than 1 or not?
+// and it's answer will be execute in boolean value (true/false)!
+
+// console.log(2 >= 1);
+//2 greater than equals to
+
+// console.log(2 < 1);
+//2 less than 1
+
+// console.log(2 == 1);
+//here we compare equals
+
+// console.log(2 != 1);
+// here we compare about "not equals"
+
+                        // or more than that about = "equals"
