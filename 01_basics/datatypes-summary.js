7160165a5370b360b57585dde0a718956928412c
Muhammad Masham Hassan <hassanmasham789@gmail.com>
2025-10-24 12:22:41 +0000

    datatype summary discussed

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,27 @@
+//Summary on Datatypes as per interview perspective!
+
+// Datatypes are divided into two parts:
+// 1: Primitive ; 2: Non-Primitive or (Reference Type)
+
+// how they both differ;
+// like how data will transfer into the memory? or how it is access?
+// by these two basis of data the PRIMITIVE and NON-PRIMITIVE datatypes are categorized!
+
+// There are 7 basic types of Primitive Datatypes:
+// 1: String
+// 2: Number
+// 3: Boolean (yes/no ; true/false)
+// 4: Null (null means empty (not a 0 or not an empty string) )
+// 5: undefined (in which we dont know what the value returns)
+// 6: Symbol (symbol is used to make any value unique)
+// 7: BigInt (it is used for scientific values or for some very higher values whcih cannot handle then we can use SYMBOL)
+
+
+// There are now few more types of Reference Type (Non-Primitive) datatype:
+// 1: Array
+// 2: Objects (if you want to master JS so you must have to master JavaScript's Objects)
+// 3: Functions
+
+
+// There's one more thing which is asked many times:
+// Is Javascript Dynamically Type Language or Statically Type Language??
